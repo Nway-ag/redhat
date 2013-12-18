eddfb749af2a675d435d8b123b8b2dbe58c7f22a
Wang Li <liwan@redhat.com>
2013-12-18 17:55:02 +0800

    push the 953225_case to beaker

--- 953225-thp-dont-respect-zone_reclaim/test_case_953225.sh
+++ 953225-thp-dont-respect-zone_reclaim/test_case_953225.sh
@@ -26,8 +26,9 @@
 # # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 cur_path=$(pwd)
-FAIL_COUNT=-1
-
+zone_reclaim_mode=
+drop_caches=
+thp_enabled="always"
 function compile_tools()
 {
 	### compile the C program ###
@@ -37,7 +38,7 @@ function compile_tools()
 			gcc -o usemem usemem.c
 		fi
 	else
-		echo "sorry,$(pwd)breakthp.c or $(pwd)usemem.c not exist."; 
+		echo "LOGINFO: Sorry,$(pwd)breakthp.c or $(pwd)usemem.c not exist."; 
 	fi
 }
 
@@ -46,54 +47,69 @@ function setup_test_env()
 	compile_tools
 
 	### set the env for testing ###
-	echo 1 >/proc/sys/vm/zone_reclaim_mode
-	echo 3 >/proc/sys/vm/drop_caches
-	echo never >/sys/kernel/mm/transparent_hugepage/enabled
+	zone_reclaim_mode=`cat /proc/sys/vm/zone_reclaim_mode`
+	echo 1 > /proc/sys/vm/zone_reclaim_mode
+
+	drop_caches=`cat /proc/sys/vm/drop_caches`
+	echo 3 > /proc/sys/vm/drop_caches
+
+	echo never > /sys/kernel/mm/transparent_hugepage/enabled
+}
+
+function cleanup()
+{
+	echo "$zone_reclaim_mode" > /proc/sys/vm/zone_reclaim_mode
+	echo "$drop_caches" > /proc/sys/vm/drop_caches
+	echo "$thp_enabled" > /sys/kernel/mm/transparent_hugepage/enabled
 }
 
 function test_running()
 {
 	### running the C program to testing ###
 	numa_node=`numactl -H | sed -n '2p' | cut -b 14-18 && numactl -H | sed -n '5p' | cut -b 14-18` 
+
 	for i in $numa_node; do
-		numactl --physcpubind=$i ./breakthp 1024 2048 4 60   &
+		echo "LOGINFO: Start to run $ numactl --physcpubind=$i ./breakthp 1024 2048 4 60"
+		numactl --physcpubind=$i ./breakthp 1024 2048 4 60  2&>1 > /dev/null &
 	done
 
-	echo "sleeping 1..."
+	echo "LOGINFO: sleeping 1..."
 	sleep 1
 
+	echo
 	for i in $numa_node; do
-		numactl --physcpubind=$i ./usemem 1024 10  &
+		echo "LOGINFO: Start to run $numactl --physcpubind=$i ./usemem 1024 10"
+		numactl --physcpubind=$i ./usemem 1024 10 2&>1 > /dev/null &
 	done
 }
 
 function check_the_output()
 {
 	### to check the numa-maps output ###
-	sleep 3
-	./numa-maps -n usemem  > test_case_report.txt
+	echo
+	echo "LOGINFO: Sleeping 5s to wait usemem"
+	sleep 5
+
+	echo "LOGINFO: The output of ./numa-maps -n usemem"
+	./numa-maps -n usemem  | tee test_case_report.txt
 
 	### to judge the data ###
-	for i in 2 3 4 5 6; do
+	for i in `seq 2 7`; do
 
 		N0=`awk '{print $6}' test_case_report.txt | sed -n ''$i'p'`
 
 		if [ "$N0" = "1.00G" ]; then
-			FAIL_COUNT=$((FAIL_COUNT + 1));
+			continue
+		elif [ "$N0" = "0" ]; then
+			continue
 		else
-			if [ "$N0" = "0" ]; then
-				FAIL_COUNT=$((FAIL_COUNT + 1));
-			fi
+			echo "FAIL: Test case failed"
+			cleanup
+			exit -1
 		fi
 	done
 
-	### here print the result of cases ###
-	if [ $FAIL_COUNT -ne 4 ]; then
-		echo "Failed:FAIL_COUNT is/are: $FAIL_COUNT, cases failed."
-	else
-		echo "Success: Test Pass!"
-
-	fi
+	echo "LOGINFO: Test case PASS!"
 }
 
 function main()
@@ -101,6 +117,7 @@ function main()
 	setup_test_env
 	test_running
 	check_the_output
+	cleanup
 
 	return 0
 }
