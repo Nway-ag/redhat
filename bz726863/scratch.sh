860530fc0e0dab8545b5d060590772a980deeddb
Levi Wang <wangli5665@gmail.com>
2014-02-19 22:32:00 +0800

    update the scratch code
    Signed-off-by: Levi Wang <wangli5665@gmail.com>

--- bz726863/scratch.sh
+++ bz726863/scratch.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
 for i in `seq 1 105`; do
-	link=`cat bz726863_patch.txt | sed -n ''$i'p'`
-	wget $link
-	ls -l | awk '{print $9}' |wc
+	link="`cat bz726863_patch_links.txt | sed -n ''$i'p' | awk '{print $3}'`/raw"
+	wget -P ./download_patch $link
+#	ls -l | awk '{print $9}' |wc
+done
+
+
+cd download_patch;
+for i in `seq 2 105`; do
+	patch=`ls -l | sed -n ''$i'p' | awk '{print $9}'`
+	cat $patch | grep '+'
+	
+	if [grep "x86_64" -eq " "];then
+		cat $patch | grep '+' | sed -n '3p' | awk '{print $2}' >> ../abi_x86_64.txt
+	fi
 
 done
