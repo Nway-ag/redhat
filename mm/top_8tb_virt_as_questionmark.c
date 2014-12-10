752f4ee6714555ecec742f8c7162df17462e05f6
Li Wang <liwang@redhat.com>
2014-12-10 10:58:11 +0800

    add two mm testcase
    Signed-off-by: Li Wang <liwang@redhat.com>

---
+++ mm/top_8tb_virt_as_questionmark.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <malloc.h>
+
+int main(void)
+{ void *m;
+	int i;
+
+	printf("Allocation VIRT memory\n");
+	printf("top shows a questionmark for VIRT when it is larger than 8TB\n");
+
+	for(i=0; i<200; i++) {
+		m=malloc(1L*50*1024*1024*1024);
+		printf("malloc returned %p\n",m);
+		if(i>161) {
+			printf("press return to for next allocation\n");
+			getchar();
+		}
+	}
+}
